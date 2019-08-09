f157381b760178631c440d847cf2c2e4ff47ec39
Suman Banerjee <suman.banerjee4@cognizant.com>
2019-08-09 11:04:58 +0100

    Merged literal intent into a single intent

--- handlers.js
+++ handlers.js
@@ -4,29 +4,44 @@ let salesforce = require("./salesforce");
 
 exports.CreateCase = (slots, session, response) => {
     session.attributes.stage = "ask_subject";
+    session.withShouldEndSession(false);
     response.ask("OK,Please help me with some information. Tell me the subjcet for the case");
 };
 
-exports.AnswerSubject = (slots, session, response) => {
+exports.AnswerLiteral = (slots, session, response) => {
     if (session.attributes.stage === "ask_subject") {
         session.attributes.subject = slots.Subject.value;
         session.attributes.stage = "ask_description";
         response.ask("Allright! Now please give me brief description of your problem");
-    } else {
+    } 
+    else if (session.attributes.stage === "ask_description") {
+        session.attributes.description = slots.Description.value;
+        session.attributes.stage = "ask_priority";
+        response.ask("What is priority for this ? High, Medium or Low");
+    }
+    else if (session.attributes.stage === "ask_priority") {
+        let priority = slots.Priority.value;
+        session.attributes.priority = priority;
+        //build salesforce logic here
+        salesforce.createCase({subject: session.attributes.subject, description: session.attributes.description, priority: session.attributes.priority})
+            .then(createdCase => {
+                if (createdCase) {
+                    let text = `Now relax, your case has been logged succesfully. Your case number is ${createdCase.get("CaseNumber")}`;
+                    response.say(text);
+                } else {
+                    response.say(`Sorry, I was not able to create your case. Please take a coffee and come back`);
+                }
+            })
+            .catch((err) => {
+                console.error(err);
+                response.say("Oops. Something went wrong");
+            });
+    }  
+    else{
         response.say("Sorry, I didn't understand that");
     }
 };
 
-exports.AnswerDescription = (slots, session, response) => {
-    if (session.attributes.stage === "ask_description") {
-        session.attributes.description = slots.Description.value;
-        session.attributes.stage = "ask_priority";
-        response.ask("What is priority for this ? High, Medium or Low");
-    } 
-    else {
-        response.say("Sorry, I didn't understand that");
-    }
-};
     
 exports.AnswerPriority = (slots, session, response) => {    
     if (session.attributes.stage === "ask_priority") {
